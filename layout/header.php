f1aa4faa2025e11bc86dc710abbab1eac47e8ff2
Taras Holets <taras.holets@gmail.com>
2016-06-13 18:43:29 +0300

    first commit

---
+++ layout/header.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+		
+		<title>Dnipro LLC</title>
+
+		<link href="css/normalize.css" rel="stylesheet">
+		<link href="css/style.css" rel="stylesheet">
+        <link rel="stylesheet" href="css/iphone4.css" media="only screen and (max-width:320px)">
+        <link rel="stylesheet" href="css/iphone480.css" media="only screen and (max-width:480px)">
+		<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
+		<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.1.5/assets/owl.carousel.min.css">
+		<script type="text/javascript" src="js/script.js"></script>
+	</head>
+	   <body>
+            <main id="content">
+                <header class="root">
+                    <div class="container">
+                        <div class="clearfix">
+                            <div>
+                                <img class="logo" src="images/logo.png" alt="">
+                            </div>
+                            <div>
+                                <img class="clook" src="images/clook.png" alt="">
+                            </div>    
+                            <div class="top">
+                                <nav class="primary">
+                                    <ul>
+                                        <li><a href="#"><img class="image-top" src="images/Kabinet.png">Кабінет</a></li>
+                                        <li><a href><img class="image-top" src="images/map.png">Трекінг</a></li>
+                                        <li><a href="#"><img class="image-top" src="images/calculator.png">Вартість</a></li>
+                                        <li><a href="#"><img class="image-top" src="images/help.png">Підтримка</a></li>
+                                    </ul>              
+                                </nav>
+                            </div>                        
+                            <div class="botton">
+                                <nav class="botton-nav">
+                                    <ul>
+                                        <li><a href="#">Сервіси <i class="fa fa-angle-down" aria-hidden="true"></i></a>
+                                            <ul>
+                                                <li><a href="#">Авіа Перевезення</a></li>
+                                        <li><a href="#">Морські Перевезення <i class="fa fa-angle-right" aria-hidden="true"></i></a>
+                                            <ul>
+                                                <li><a href="#">Контейнерні</a></li>
+                                                <li><a href="#">Ro-Ro/Lo-Lo</a></li>
+                                            </ul>
+                                        </li>  
+                                                <li><a href="#">Перевезення по США</a></li>
+                                        <li><a href="#">Доставка посилок з США <i class="fa fa-angle-right" aria-hidden="true"></i></a>
+                                            <ul class="but-meny">
+                                                <li><a href="#">Доставка в Україну</a></li>
+                                                <li><a href="#">Доставка в Росію</a></li>
+                                                <li><a href="#">Доставка в Білорусь</a></li>
+                                                <li><a href="#">Доставка в Молдову</a></li>
+                                                <li><a href="#">Доставка в Грузію</a></li>
+                                                <li><a href="#">Доставка в країни Євросоюзу</a></li>
+                                                <li><a href="#">Доставка в країни Середньої Азії</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="#">Складські послуги <i class="fa fa-angle-right" aria-hidden="true"></i></a>
+                                            <ul class="poslugi">
+                                                <li><a href="#">Адреса в США</a></li>
+                                                <li><a href="#">Зберігання</a></li>
+                                                <li><a href="#">Консолідація</a></li>
+                                                <li><a href="#">Перепакування</a></li>
+                                            </ul>
+                                                </li>
+                                            </ul>
+                                        </li>                            
+                                        <li><a href="#"><i class="fa fa-shopping-cart" style="color: #a6ce39" aria-hidden="true"></i>  Amazon&amp;eBay</a></li>        <li><a href="#"><i class="fa fa-car" style="color: #a6ce39" aria-hidden="true"></i>  Авто-мото <i class="fa fa-angle-down" aria-hidden="true"></i></a>
+                                            <ul>
+                                                <li><a href="#">Доставка Автомобілів</a></li>
+                                                <li><a href="#">Доставка Мотоциклів </a></li>
+                                                <li><a href="#">Доставка Лодок </a></li>
+                                                <li><a href="#">Перевезення авто по США </a></li>
+                                                <li><a href="#">Доставка Спец.Техніки </a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="#">Блог</a></li>
+                                        <li><a href="#">Контакти</a></li>
+                                        <li><a href="#"><i class="fa fa-search" style="color: #a6ce39" aria-hidden="true"></i></a></li>
+                                        <li><a href="#" style="color: #0072bc">укр</a></li>
+                                        <li><a href="#" style="color: #0072bc">рус</a></li>
+                                        <li><a href="#" style="color: #0072bc">eng</a></li>
+
+                                    </ul>
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                </header>
+            </main>
+
+
+<script>
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1){
+            $('header').addClass("sticky");
+     }
+        else{
+            $('header').removeClass("sticky");
+     }
+    });
+</script>
+
+
+
+	
+
+	
+
+
+
+
+
+
+
